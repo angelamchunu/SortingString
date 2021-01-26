35ce889e6da756db40e67d9747496959b875fe70
angela.mchunu <angela.mchunu@standardbank.co.za>
2021-01-26 03:37:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Sorter sorter = new Sorter();
+            var sorted = sorter.Sort("Contrary to popular belief, the pink unicorn flies east.");
+
+            foreach (KeyValuePair<char, int> entry in sorted)
+            {
+                for (int i = 0; i < entry.Value; i++)
+                {
+                    Console.Write(entry.Key);
+                }
+            }
+        }
+    }
+}
---
+++ Sorter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SortingString
+{
+   public  class Sorter
+    {
+
+        public SortedDictionary<char,int> Sort(string inputString)
+        {
+            inputString = Regex.Replace(inputString, "[^\\w\\d]","");
+            inputString = Regex.Replace(inputString, " ", "");
+            char[] charArray = inputString.ToLower().ToCharArray();
+
+            SortedDictionary<char, int> sortedDictionary = new SortedDictionary<char, int>();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (sortedDictionary.ContainsKey(charArray[i]))
+                {
+                    int val = sortedDictionary[charArray[i]];
+                    sortedDictionary.Remove(charArray[i]);
+                    sortedDictionary.Add(charArray[i], val + 1);
+                }
+                else
+                    sortedDictionary.Add(charArray[i], 1);
+            }
+
+            return sortedDictionary;
+        }
+
+     
+    }
+}
